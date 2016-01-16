d3e1a654131bf57b19dbb2a201bf931f4ce0f871
Raf <7909000@gmail.com>
2016-01-17 02:37:48 +0500

    update

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,13 +3,20 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+var autoprefixer = require('gulp-autoprefixer');
+
 gulp.task('sass', function () {
     gulp.src('./src/uikit.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css'))
+        .pipe(autoprefixer())
         .pipe(gulp.dest('./examples'));
 });
 
 gulp.task('sass:watch', function () {
     gulp.watch('./src/**/*.scss', ['sass']);
 });
+
+gulp.task('default', [
+    'sass:watch'
+]);
